673ae6d7d6157b328d97d4967e4e0d2277bc25b2
Charles de Villiers <chasdev@yahoo.com>
2020-06-07 23:15:54 +0200

    Learning something about Bootstrap...

--- dev/js/chess_p30_site.js
+++ dev/js/chess_p30_site.js
@@ -1,4 +1,4 @@
-$ (function () {
+$(function () {
 
   var board = null;
   var promoting = false;
---
+++ dev/js/clock.js
@@ -0,0 +1,3 @@
+$ (function () {
+//   alert ('Clock code is ready!');
+}); // End of jQuery ready()
